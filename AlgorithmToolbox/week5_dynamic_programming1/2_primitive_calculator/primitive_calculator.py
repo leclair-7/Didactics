e7550ddd4028fd740ee397fdcb9d88e4717e8219
leclair-7 <andromeda.t@tutanota.com>
2018-08-12 22:39:10 -0500

    Primitive calculator problem DP, submitted successfully on the first try

--- AlgorithmToolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ AlgorithmToolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -3,21 +3,66 @@ import sys
 
 def optimal_sequence(n):
     sequence = []
+
+    arr = [ i-1 for i in range(n+1) ]
+
+    index = 0
+    for i in range(n+1):
+        addone = -1
+        mul2   = -1
+        mul3   = -1
+
+        if i * 3 <= n:
+            mul3 = i * 3
+            if arr[i] + 1 < arr[mul3]:
+                arr[mul3] = arr[i] + 1
+
+        if i * 2 <= n:
+            mul2 = i * 2
+            if arr[i] + 1 < arr[mul2]:
+                arr[mul2] = arr[i] + 1
+
+        if i + 1 <= n:
+            addone = i + 1
+            if arr[i] + 1 < arr[addone]:
+                arr[addone] = arr[i] + 1
+    sequence = [n]
     while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
-        else:
-            n = n - 1
+        
+        subone = -1
+        div2   = -1
+        div3   = -1
+        #it's not quite this, but this is sooo close, question is how is it related to coin change problem?
+        #the next number is the min( arr[i//2], arr[i//3], arr[i-1] ), but only the ones that work
+        div3 = n // 3 if n % 3 == 0 else 10**7
+        div2 = n // 2 if n % 2 == 0 else 10**7
+        subone = n - 1
+
+        candidates = [i for i in [subone,div2,div3] if i != 10**7]
+        #print(candidates)
+        #thenextseq = min( candidates  )
+        
+        cans = []
+        for i in candidates:
+            cans.append(arr[i])
+
+        n = candidates[cans.index(min(cans))]
+
+        #print(n, candidates, cans)
+        #print(arr)
+        #n = thenextseq
+        sequence.append( n )
+
+    #print(arr[:15])
+    sequence = sequence[:-1]
     return reversed(sequence)
-'''
+
 input = sys.stdin.read()
 n = int(input)
-'''
-n=96234
 sequence = list(optimal_sequence(n))
 print(len(sequence) - 1)
 for x in sequence:
     print(x, end=' ')
+'''
+print(optimal_sequence(1))
+'''
--- AlgorithmToolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ AlgorithmToolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,7 +1,26 @@
 # Uses python3
-def edit_distance(s, t):
-    #write your code here
-    return 0
+def edit_distance(A,B):
+    D = [[999 for j in range(len(B))] for i in range(len(A))]
+
+    #print(len(D[0]))
+    for i in range(len(B)):
+        D[0][i] = i
+    for i in range(len(A)):
+        D[i][0] = i
+    #pprint(D)
+
+    for j in range(1, len(B)):
+        for i in range(1, len(A)):
+            insertion = D[i][j-1] + 1 
+            deletion  = D[i-1][j] + 1
+            match     = D[i-1][j-1]
+            mismatch  = D[i-1][j-1] + 1
+            if A[i] == B[j]:
+                D[i][j] = min(insertion, deletion, match) 
+            else:
+                D[i][j] = min(insertion, deletion, mismatch)
+    return D[len(A)-1][len(B)-1]
 
 if __name__ == "__main__":
-    print(edit_distance(input(), input()))
+    #print(edit_distance(input(), input()))
+    print(edit_distance("short","ports"))
